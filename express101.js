9fd65da0041e1af031694f43fd5ffafe74edcc8e
Kevin Farmer <farmekp0@sewanee.edu>
2016-12-06 14:16:14 -0500

    Second commit.

--- express101.js
+++ express101.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var myApp = express();
 
+myApp.use(express.static('public'));
+
 myApp.set('view engine', 'hbs');
 
 myApp.get('/', function (req, res) {
   res.send('Hello, world!');
 });
 
-myApp.get('/year', function(req, res) {
+myApp.get('/name/:name/age/:age', function(req, res) {
   var today = new Date();
   var year = today.getFullYear();
-  var yearBorn = year - req.query.age;
-  res.send('You were born in ' + yearBorn);
+  var yearBorn = year - req.params.age;
+  var name = req.params.name;
+  res.render('hello.hbs', {
+    title: 'Hiya',
+    name: name,
+    yearBorn: yearBorn
+  });
 });
 
 myApp.listen(3000, function () {
